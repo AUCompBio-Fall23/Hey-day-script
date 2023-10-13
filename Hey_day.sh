4cfc63f2cdc76877527823f340836077df58d9e4
Damseltemi <111428859+Damseltemi@users.noreply.github.com>
2023-10-13 09:40:24 -0500

    Add files via upload

---
+++ Hey_day.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+# writing script for Hey day
+echo "Enter your name"
+
+# read assign name by the user 
+read name
+
+# print user name with Hey 
+echo "Hey,$name"
+echo "Celebrating Hey_day in Auburn on October 18th"  
